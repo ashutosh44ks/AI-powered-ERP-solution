844d0212e7ce134037a361ecbe3418dc508744cd
Ashutosh Singh <ashutosh.singh@oodles.io>
2025-09-01 13:10:16 +0530

    feat: implement DataTable component and integrate it into TabularInteraction page

--- client/src/pages/TabularInteraction.tsx
+++ client/src/pages/TabularInteraction.tsx
@@ -1,12 +1,36 @@
+import { DataTable } from "@/components/ui/data-table";
+import dataModels from "@/services/dataModels";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 
 const TabularInteraction = () => {
   const { tableName } = useParams();
-  if (!tableName) return null;
-  return <div>
-    <h3 className="text-xl mb-4">Now Showing: "{tableName}"</h3>
-    
-  </div>;
+  const { data: tableData } = useQuery({
+    queryKey: ["tableData", tableName],
+    queryFn: () => dataModels.getTableData(tableName),
+    enabled: !!tableName,
+  });
+  const { data: tableConfig } = useQuery({
+    queryKey: ["tableConfig", tableName],
+    queryFn: () => dataModels.getTableConfig(tableName),
+    enabled: !!tableName,
+    staleTime: Infinity,
+  });
+
+  if (!tableName)
+    return (
+      <h3 className="text-xl mb-4">
+        Error fetching details for: "{tableName}"
+      </h3>
+    );
+  return (
+    <div>
+      <h3 className="text-xl mb-4">Now Showing: "{tableName}"</h3>
+      {tableConfig?.data && tableData?.data && (
+        <DataTable columns={tableConfig.data} data={tableData.data} />
+      )}
+    </div>
+  );
 };
 
 export default TabularInteraction;
--- client/src/services/dataModels.ts
+++ client/src/services/dataModels.ts
@@ -1,5 +1,6 @@
 import api from "@/lib/api";
 import type { APIResponse, DbSchema } from "@/lib/constants";
+import type { ColumnDef } from "@tanstack/react-table";
 
 const saveRecord = async (formData: FormData) => {
   const response = await api.post(`/ai/save-record`, formData, {
@@ -18,7 +19,24 @@ const getDBTables = async (): Promise<APIResponse<DbSchemaMap>> => {
   return response.data;
 };
 
+const getTableConfig = async (
+  tableName: string | undefined
+): Promise<APIResponse<ColumnDef<unknown>[]>> => {
+  if (!tableName) throw new Error("Table name is required");
+  const response = await api.get(`/data-models/${tableName}/config`);
+  return response.data;
+};
+const getTableData = async (
+  tableName: string | undefined
+): Promise<APIResponse<unknown[]>> => {
+  if (!tableName) throw new Error("Table name is required");
+  const response = await api.get(`/data-models/${tableName}/data`);
+  return response.data;
+};
+
 export default {
   saveRecord,
   getDBTables,
+  getTableConfig,
+  getTableData,
 };
